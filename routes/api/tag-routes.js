8822b936fa84c97bca4da040cbedcfdc61e69524
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-08-19 23:44:21 -0500

    added through: property in get routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,7 +6,8 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product }],
+      include: [{ model: Product, 
+      through: ProductTag}],
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -21,7 +22,8 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product}]
+      include: [{ model: Product,
+        through: ProductTag}]
     });
 
     if (!tagData) {
