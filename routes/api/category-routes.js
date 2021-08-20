61cd34dfce60770bf685bae8ed4588c0d9f44e94
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-08-19 21:37:07 -0500

    added delete route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -62,8 +62,16 @@ router.put('/:id', async(req, res) => {
 });
 
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: { id: req.params.id }
+    });
+    res.status(200).json({ message: 'This category has been Deleted!' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
